2428944832659211cd3bbb47663185c024fd9dfe
husna <husnajaved.241@gmail.com>
2021-06-12 16:20:09 +0500

    initial commit

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+	'ENGINE': 'django.db.backends.postgresql',
+	'NAME': 'restAPI_DB',
+	'USER': 'postgres',
+	'PASSWORD': 'admin',
+	'HOST': '127.0.0.1',
+	'PORT': '5432',
+	}
 }
 
 
